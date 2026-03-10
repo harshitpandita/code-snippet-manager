2abb65d3999cb70f4939cf49b8ec0be4f2d59a3e
harshitpandita <harshit.pandita2111@gmail.com>
2026-03-10 11:48:03 +0530

    Added image versioning and health endpoint

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,24 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Code Snippet Manager API Running");
 });
+
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from backend API" });
 });
 
+/* HEALTH CHECK ENDPOINT */
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    service: "snippet-backend",
+    timestamp: new Date()
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/snippets").then(() => console.log("MongoDB connected"))
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/snippets")
+.then(() => console.log("MongoDB connected"))
 .catch(err => console.log(err));
 
 app.use("/snippets", snippetRoutes);
