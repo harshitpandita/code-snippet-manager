2ff2bf7bc7bfd3fc4e49939efca7a9d09d4ae63c
harshitpandita <harshit.pandita2111@gmail.com>
2026-03-10 11:13:39 +0530

    Add GitHub Actions CI pipeline

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,7 @@ app.get("/api", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect("mongodb://localhost:27017/snippetdb")
-.then(() => console.log("MongoDB connected"))
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/snippets").then(() => console.log("MongoDB connected"))
 .catch(err => console.log(err));
 
 app.use("/snippets", snippetRoutes);
